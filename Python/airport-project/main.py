88097d3da29cefbbb9e80e66746fda6118d43349
TakaIzuki <isaacjroberts0505@gmail.com>
2021-03-03 15:02:24 +0000

    Update main.py
    Created main() function and ported the CSV file into a list.

--- Python/airport-project/main.py
+++ Python/airport-project/main.py
@@ -2,8 +2,23 @@
 
 
 
-f = open("Python/airport-project/Airports.txt", "r")
 # Functions
+
+# Main execution of the program.
+def main():
+    # Open provided CSV file
+    f = open("Python/airport-project/Airports.txt", "r")
+    # Define list to store seperated CSV file in.
+    airports_list = []
+
+    # Loop through file and split each line into seperate lists.
+    for i in f:
+        split_line = i.split(",")
+        # Append the seperate lists into one list to create a listed version of our CSV file.
+        airports_list.append(split_line)
+
+    print(airports_list)
+
 # Kill program when called
 def exitProgram():
     exit()
@@ -20,4 +35,4 @@ Choose an option by typing the respective number:
         '''
     )
 
-print(f.read())
+main()
